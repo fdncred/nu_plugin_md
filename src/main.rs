4272c9c553d6c7c4eab37ee0164906f0b3e7af22
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-06-21 06:43:35 -0500

    update to latest nushell version

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use termimad::*;
 pub struct MdPlugin;
 
 impl Plugin for MdPlugin {
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
+
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(Implementation)]
     }
